0c41ee06f9475b971341f31c018094a2f97d9bac
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2024-02-24 22:26:55 -0300

    feat: is borning

---
+++ src/main.rs
@@ -0,0 +1,78 @@
+use raylib::{
+    ffi::{exit, CloseWindow, GetCharPressed},
+    prelude::*,
+};
+use std::{any::Any, char::from_u32, collections::HashSet, process::Command};
+
+const FONT_SIZE: i32 = 30;
+
+pub fn main() {
+    let output = Command::new("bash")
+        .args(["-c", "compgen -c"])
+        .output()
+        .unwrap()
+        .stdout;
+
+    let bindings = String::from_utf8(output).unwrap();
+    let programs: HashSet<&str> = bindings.lines().collect();
+    let programs: Vec<&str> = programs.into_iter().collect();
+
+    let mut current_programs = programs.clone();
+
+    let (mut rl, thread) = raylib::init().size(600, 800).title("Finsk").build();
+
+    let mut search_bar = String::from("Hello");
+
+    let mut selected_program = 0;
+
+    unsafe {
+        while !rl.window_should_close() {
+            let pressed_key = rl.get_key_pressed();
+            let mut d = rl.begin_drawing(&thread);
+
+            d.clear_background(Color::BLACK);
+
+            d.draw_rectangle(0, 0, 600, FONT_SIZE + 10, Color::LIGHTGRAY);
+            d.draw_text(&search_bar, 10, 5, FONT_SIZE, Color::BLACK);
+
+            for (i, program) in current_programs.iter().enumerate() {
+                let y = (i as i32 * FONT_SIZE) + 50;
+
+                if i == selected_program {
+                    d.draw_rectangle(0, y, 600, FONT_SIZE, Color::LIGHTGRAY);
+                    d.draw_text(program, 10, y, FONT_SIZE, Color::BLACK);
+                } else {
+                    d.draw_text(program, 10, y, FONT_SIZE, Color::WHITE);
+                }
+            }
+
+            let mut key = GetCharPressed();
+
+            while key > 0 {
+                search_bar.push(char::from_u32(key as u32).unwrap());
+                key = GetCharPressed();
+            }
+
+            match pressed_key {
+                Some(raylib::consts::KeyboardKey::KEY_BACKSPACE) => {
+                    search_bar.pop();
+                }
+                Some(raylib::consts::KeyboardKey::KEY_ENTER) => {
+                    Command::new("bash")
+                        .args(["-c", &current_programs[selected_program]])
+                        .spawn()
+                        .unwrap();
+
+                    exit(0);
+                }
+                _ => {}
+            }
+
+            current_programs = programs
+                .iter()
+                .filter(|program| program.contains(&search_bar))
+                .copied()
+                .collect();
+        }
+    }
+}
